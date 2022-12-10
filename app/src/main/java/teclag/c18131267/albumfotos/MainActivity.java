9c781deacaef280cb8dc562cc780a21689586262
Alberto <pqdstar_alberto@live.com>
2022-12-10 00:05:37 -0600

    Esqueleto de la aplicacion terminado

--- app/src/main/java/teclag/c18131267/albumfotos/MainActivity.java
+++ app/src/main/java/teclag/c18131267/albumfotos/MainActivity.java
@@ -1,29 +1,51 @@
 package teclag.c18131267.albumfotos;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int CODIGO_CAPTURA_FOTO = 123;
+    private Uri uriFoto;
+    private String ruta;
 
     ArrayList<Integer> imagenes = new ArrayList<>(Arrays.asList(
             R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,
             R.drawable.img5,R.drawable.img6,R.drawable.img7,R.drawable.img8,
             R.drawable.img9,R.drawable.img10,R.drawable.img11,R.drawable.img12
             ));
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,5 +98,30 @@ public class MainActivity extends AppCompatActivity {
         });
         dialog.show();
     }
-    
+
+    public void onClick(View view) {
+        Snackbar.make(view, "Foto", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+
+        if (id == R.id.id_AcercaDe){
+            ruta = "android.resource://" + this.getPackageName() + "/" + R.raw.gansito;
+        } else if (id == R.id.id_Navidad){
+            ruta = "android.resource://" + this.getPackageName() + "/" + R.raw.navidad;
+        }
+        lanzarVideoActiviy();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void  lanzarVideoActiviy () {
+        Intent intent = new Intent ( this, VideoActivity.class ) ;
+        intent.putExtra ( "rutaVideo", ruta );
+        startActivity ( intent );
+    }
 }
---
+++ app/src/main/java/teclag/c18131267/albumfotos/SplashActivity.java
@@ -0,0 +1,26 @@
+package teclag.c18131267.albumfotos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //se simula la pantalla de carga
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
